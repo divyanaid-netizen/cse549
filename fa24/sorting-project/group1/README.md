043217176f63467e7977c8dd7040027474e3f8c8
Paul Gao <gaozihou@uw.edu>
2024-12-05 12:56:04 -0800

    Update group 1

--- fa24/sorting-project/group1/README.md
+++ fa24/sorting-project/group1/README.md
@@ -1,4 +1,22 @@
-Current Performance:
 
-TBA
+Rajeev B. Botadra
+CSE549 Autumn 2024
+Professor Taylor
 
+# Final Project: Parallelized Sorting Kernel
+
+## Algorithm 
+
+The current kernel is a naive bubble sort algorithm that partitions the array into 256 sectors and assigns each sector to a compute tile. The sorted sectors are then joined into arrays double the size and resorted (only utiling half the tiles as the sort completes over the entire array). The loop is not unrolled and the algorithm is very poorly optimized.
+
+## Cycle Counts & Geomean
+| Size      | Cycle Count   |
+| --------- | ------------- |
+| 2^14      | 223,604       |
+| 2^16      | 1,201,118     |
+| 2^18      | 20,543,012   |
+| 2^20      | DNF           |
+| 2^22      | DNF           |
+| 2^24      | DNF           |
+
+Geomean = 1,767,026.738 Cycles
--- fa24/sorting-project/group1/kernel.cpp
+++ fa24/sorting-project/group1/kernel.cpp
@@ -28,28 +28,48 @@ kernel_sorter(float * A, int N) {
   bsg_cuda_print_stat_kernel_start();
 
   /*Each tile sorts a subsection of the vector*/
-  int len = N / (bsg_tiles_X*bsg_tiles_Y);
-  float *myA = &A[__bsg_id*len];
+  int N_subarray = N / (bsg_tiles_X*bsg_tiles_Y);
+  float *myA = &A[__bsg_id*N_subarray];
 
-  /*Unroll the loop by a factor of 0*/
-  for (int i = 0; i < len; i+=1) {
-    /*Load all operands*/
-    register float A00 = myA[i+0];
-    bsg_fence();  /*Make loads non-blocking*/
-    register float A01 = myA[i+1];
-    register float temp = A00;
-
-    asm volatile("": : :"memory");
-    /*Simple if-else statement for comparison*/
-    if(A00 > A01){
-      A00 = A01;
-      A01 = temp;
+  /*Unroll the loop by a factor of 8*/
+  /*Uses Bubble Sort, largest values sorted first*/
+  /*Outer loop converges subrarrays as they are sorted
+    and performs sort over larger converged subarrays. 
+    Not all tiles are utilized as the subarray size 
+    increases, unutilized tiles are forced to idle
+    by setting array ptr to NULL.*/
+  while(N_subarray < N){
+    if(A == 0){
+      continue;
     }
+    for (int i = 0; i < N_subarray; i++) {
+      for(int j = 0; j < N_subarray-i-1; j++){
+        /*Load all operands*/
+        register float A00 = myA[j+0];
+        bsg_fence();  /*Make loads non-blocking*/
+        register float A01 = myA[j+1];
+        register float temp = A00;
 
-    /*Store all results*/
-    myA[i+0] = A00;
-    bsg_fence();  /*Make stores non-blocking*/
-    myA[i+1] = A01;
+        asm volatile("": : :"memory");
+        /*Simple if-else statement for comparison*/
+        if(A00 > A01){
+          A00 = A01;
+          A01 = temp;
+        }
+      
+        /*Store results*/
+        myA[j+0] = A00;
+        bsg_fence();  /*Make stores non-blocking*/
+        myA[j+1] = A01;
+      }
+    }
+    N_subarray *= 2;
+    if(__bsg_id * N_subarray <= N){
+      myA = &A[__bsg_id*N_subarray];
+    }
+    else{
+      myA = 0;
+    }
   }
 
   bsg_fence();
